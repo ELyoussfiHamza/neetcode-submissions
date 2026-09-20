b52f9b99087331dfaf2b8f85e4412a1d323ac4be
HAMZA <elyoussfiihaamza@gmail.com>
2026-09-20 06:21:35 +0100

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string current;
+        backtrack(ans ,n , n , current);
+
+        return ans;
+    }
+
+    void backtrack(vector<string>& ans, int open , int close , string current){
+       
+        if (open == 0 && close == 0){
+            
+            ans.push_back(current);
+            return;
+        }
+        if (open<=close){
+            if (open>0){
+                backtrack(ans , open-1 , close , current+'(');
+            }
+            if (close > 0){
+                backtrack(ans , open , close - 1 , current + ')');
+            }
+        }
+    }
+};
